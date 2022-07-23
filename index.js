bb11131f2af506f3d82c7306a71ab81ee4d8ec29
Karen Palacio <karen.palacio.1994@gmail.com>
2022-07-22 21:56:13 -0300

    probando. no anda todavia

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import Parallax from 'parallax-js'
 var scene = document.getElementById('scene');
 
-var parallaxInstance = new Parallax(scene);
-parallaxInstance.friction(0.2, 0.2);
 
+function callback () { console.log(parallaxInstance.depthsX)};
+
+var parallaxInstance = new Parallax(scene,{ onLoad: callback });
+var first = document.getElementById("first");
+first.addEventListener('change', function(event) {
+    console.log(parallaxInstance.depthX);
+  });
+parallaxInstance.friction(0.2, 0.2);
 
