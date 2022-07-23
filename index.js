aa942db49760c1e03b95408c5b3ce1657d36b56a
Karen Palacio <karen.palacio.1994@gmail.com>
2022-07-22 22:34:18 -0300

    parallax nos sirve para animar cosas pero no nos da info de tilt ni de nada. usar otra libreria (sense) para eso

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import Parallax from 'parallax-js'
+console.log(sense)
 var scene = document.getElementById('scene');
 
 
 function callback () { console.log(parallaxInstance.depthsX)};
-
+function addValueToPage(text) {
+    var h = document.createElement("H1");
+    var t = document.createTextNode(text);
+    h.appendChild(t);
+    document.body.appendChild(h);
+}
 var parallaxInstance = new Parallax(scene,{ onLoad: callback });
 var first = document.getElementById("first");
-first.addEventListener('change', function(event) {
-    console.log(parallaxInstance.depthX);
-  });
 parallaxInstance.friction(0.2, 0.2);
 
+
+
+
+ var sense1 = sense.init({
+      debug: true
+    });
+console.log(sense1);
+    sense1.tilt(function(data){
+
+        console.log(data.toString())
+    })
+//var sense = new Sense();
+//console.log(sense);
+    //sense.tiltScroll(function(data){
+    //    console.log(data)
+    //    addValueToPage(data)
+    //})
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
     module.exports = {
-      entry: './index.js',
+      entry: [ './sense.js','./index.js'],
       output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.bundle.js'
