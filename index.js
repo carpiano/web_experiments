816faddf1a51cc55866147d389fede031807ee4e
Lucas Leonardo <lucasleonardo@protonmail.com>
2022-07-26 19:31:28 -0300

    limpia y ordena un poco el código

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-import Parallax from 'parallax-js'
-console.log(sense)
-var scene = document.getElementById('scene');
+import Parallax from "parallax-js";
 
+var scene = document.getElementById("scene");
 
-function callback () { console.log(parallaxInstance.depthsX)};
-function addValueToPage(text) {
-    var data = document.getElementById("data");
-    data.innerText = text;
+function callback() {
+  console.log(parallaxInstance.depthsX);
 }
-var parallaxInstance = new Parallax(scene,{ onLoad: callback });
+function addValueToPage(text) {
+  var data = document.getElementById("data");
+  data.innerText = text;
+}
+var parallaxInstance = new Parallax(scene, { onLoad: callback });
 parallaxInstance.friction(0.2, 0.2);
 
+var sense1 = sense.init({
+  debug: false,
+});
 
-
-
- var sense1 = sense.init({
-      debug: false
-    });
-console.log(sense1);
-    sense1.tilt(function(data){
-
-        addValueToPage(data.direction.toString() + "-" + data.magnitude.toString())
-        console.log(data.toString())
-    })
-//var sense = new Sense();
-//console.log(sense);
-    //sense.tiltScroll(function(data){
-    //    console.log(data)
-    //    addValueToPage(data)
-    //})
+sense1.tilt(function (data) {
+  addValueToPage(data.direction.toString() + "-" + data.magnitude.toString());
+  console.log(data.toString());
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
-    module.exports = {
-      entry: [ './sense.js','./index.js'],
-      output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
-      },
-      resolve: {
-        alias: {
-          'node_modules': path.join(__dirname, 'node_modules'),
-        }
-      }
-    };
+const path = require("path");
+
+module.exports = {
+  entry: ["./sense.js", "./index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.bundle.js",
+  },
+  resolve: {
+    alias: {
+      node_modules: path.join(__dirname, "node_modules"),
+    },
+  },
+};
